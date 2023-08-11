f893917d0afefe15fd4d1a9e571912286ce1b1d3
Jorge Arrojo <arrojorge@uoc.edu>
2023-08-11 23:37:17 +0200

    Data.parse return NaN

--- main.js
+++ main.js
@@ -14,9 +14,7 @@ const svg = d3.select("#chart")
           .attr("id", "svg")
           .attr("width", width)
           .attr("height", height)
-let elementGroup = svg .append("g").attr("id", "elementGroup").attr('transform', `translate(
-             ${margin.left},
-             ${margin.top})`)
+
 
 
 
@@ -41,20 +39,20 @@ let yAxisGroup = axisGroup
 // let yAxis = d3.axisLeft().scale(y)
 
 
-let ManX = ""
+let ManX = Date.parse('31/12/1970')
 let ManY = 0
 let MinX = Date.now()
 let MinY = 999999999999999
-d3.csv("data/ibex.csv").then(data => {
+data = d3.csv("data/ibex.csv").then(data => {
     data.map((d, index) => {
 
-      d.date = Date.parse(d.date)  
+      seconds_date = Date.parse(d.date)  
 
       if(d.date<MinX){
-            MinX = d.date
+            MinX = seconds_date
        }
        if(d.date>ManX){
-            ManX = d.date
+            ManX = seconds_date
        }
 
        if(d.close<MinY){
@@ -64,15 +62,15 @@ d3.csv("data/ibex.csv").then(data => {
             ManY = d.close
         }
 
-        d.date     = +d.date
-        d.close    = +d.close
+        // d.date     = d.date
+        // d.close    = d.close
         // d.n = +d.n
- })
+    })
 
 const x = d3.scaleLinear()
     .domain([MinX, ManX])
     // .range([rangeMin, rangeMax]);
-    .range([0, 9999999999]);
+    .range([0, parseInt(ManX)]);
 
 const y = d3.scaleLinear()
     .domain([parseInt(MinY), parseInt(ManY)])
@@ -83,14 +81,33 @@ let xAxis = d3.axisBottom().scale(x)
 let yAxis = d3.axisLeft().scale(y)
 
  // scale domain:
- x.domain(d3.extent(data.map(d => d.date))); y.domain(d3.extent(data.map(d => d.close)))
+ x.domain(d3.extent(data.map(d => d.date))); 
+ y.domain(d3.extent(data.map(d => d.close)))
  // call axes
  xAxisGroup.call(xAxis)
  yAxisGroup.call(yAxis)
- let valueClose = data.filter(d => d.close == "Valor al cierre")
+
+
+//  console.log("data",data)
+
+//  arr = data.map(a =>{return Date.parse(a.date) })
+//  console.log("arr",arr)
+
+
  // datum!
- elementGroup.datum(valueClose).append('path') .attr("id", "Valor al cierre")
- .attr("d", d3.line()
- .x(d => x(d.date))
-    .y(d => y(d.close))
- ) })
+//  elementGroup.enter().append('path')
+//  .attr("d", d3.line()
+//  .x(d => x(Date.parse(d.date)))
+//  .y(d => y(parseInt(d.close)))
+//  ) 
+
+let elementGroup = svg.append("g").selectAll("path")
+.data([data]).attr("id", "elementGroup")
+elementGroup.enter().append("path")
+    .attr("d", d3.line()
+    .x(d => (Date.parse(d.date)))
+    .y(d => (d.close)))
+    .attr("fill", "none")
+    .attr("stroke", "blue")
+})
+
