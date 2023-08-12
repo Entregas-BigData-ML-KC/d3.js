0b147ff950d7bdbbb067ee5a45083def5264f424
Jorge Arrojo <arrojorge@uoc.edu>
2023-08-12 16:37:29 +0200

    render line

--- main.js
+++ main.js
@@ -46,15 +46,16 @@ let ManX = Date.parse('01/01/1970')
 let ManY = 0
 let MinX = Date.now()
 let MinY = 999999999999999
-data = d3.csv("data/ibex.csv").then(data => {
+dataMap = d3.csv("data/ibex.csv").then(data => {
     data.map((d, index) => {
 
       const [dia, mes, año] = d.date.split('/');
       let dateObject = new Date(`${año}-${mes}-${dia}`)
       d.date = fecha(dateObject)
+     
       dateObject = Date.parse(dateObject)
       
-      console.log("DATE", dateObject)
+      //console.log("DATE", dateObject)
 
       d.close = parseFloat(d.close)
 
@@ -79,49 +80,71 @@ data = d3.csv("data/ibex.csv").then(data => {
         // d.n = +d.n
     })
 
-const x = d3.scaleLinear()
-    .domain([parseInt(MinX), parseInt(ManX)])
-    // .range([rangeMin, rangeMax]);
-    .range([0, width]);
+    // const x = d3.scaleLinear()
+    //     .domain([parseInt(MinX), parseInt(ManX)])
+    //     // .range([rangeMin, rangeMax]);
+    //     .range([0, width]);
 
-const y = d3.scaleLinear()
-    .domain([parseInt(MinY), parseInt(ManY)])
-    // .range([rangeMin, rangeMax]);
-    .range([0, height]);
-
-let xAxis = d3.axisBottom().scale(x)
-let yAxis = d3.axisLeft().scale(y)
-
- // scale domain:
- x.domain(d3.extent(data.map(d => d.date))) 
- y.domain(d3.extent(data.map(d => d.close)))
- // call axes
- xAxisGroup.call(xAxis)
- yAxisGroup.call(yAxis)
+    // const y = d3.scaleLinear()
+    //     .domain([parseInt(MinY), parseInt(ManY)])
+    //     // .range([rangeMin, rangeMax]);
+    //     .range([0, height]);
 
 
-  console.log("data",data)
+    // Crea la escala para el eje x (fechas)
+    const x = d3.scaleTime()
+    .domain(d3.extent(dataMap, d => (d.date))) // Extremos de las fechas en los datos
+    .range([margin.left, width - margin.right]);
+
+    // Crea la escala para el eje y (valores numéricos)
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(dataMap, d => d.close)]) // Rango de valores en los datos
+    .range([height - margin.bottom, margin.top]);
+
+    let xAxis = d3.axisBottom().scale(x)
+    let yAxis = d3.axisLeft().scale(y)
+
+    // scale domain:
+    x.domain(d3.extent(data.map(d => Date.parse(d.date)))) 
+    y.domain(d3.extent(data.map(d => d.close)))
+    // call axes
+    xAxisGroup.call(xAxis)
+    yAxisGroup.call(yAxis)
 
 
-//  arr = data.map(a =>{console.log("type",typeof(a.close)) })
-//  console.log("arr",arr)
+    console.log("data",data)
 
 
- // datum!
-//  elementGroup.enter().append('path')
-//  .attr("d", d3.line()
-//  .x(d => x(Date.parse(d.date)))
-//  .y(d => y(parseInt(d.close)))
-//  ) 
+     arr = data.map(a =>{console.log("type",typeof(a.date)) })
+     console.log("arr",arr)
 
-let elementGroup = svg.append("g").selectAll("path").attr("id", "elementGroup") .attr('transform', `translate(
-    ${margin.left},
-    ${margin.top})`)
-    .data([data]).attr("id", "elementGroup")
-elementGroup.enter().append("path")
-    .attr("d", d3.line()
-    .x(d => (Date.parse(d.date)))
-    .y(d => (d.close)))
-   
+
+    // datum!
+    //  elementGroup.enter().append('path')
+    //  .attr("d", d3.line()
+    //  .x(d => x(Date.parse(d.date)))
+    //  .y(d => y(parseInt(d.close)))
+    //  ) 
+
+    // let elementGroup = svg.append("g").selectAll("path").attr("id", "elementGroup") .attr('transform', `translate(
+    //     ${margin.left},
+    //     ${margin.top})`)
+    //     .data([data]).attr("id", "elementGroup")
+    // elementGroup.enter().append("path")
+    //     .attr("d", d3.line()
+    //     .x(d => x((d.date)))
+    //     .y(d => y(d.close)))
+    
+
+    let elementGroup = svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2)
+        .attr("d", d3.line()
+            .x(d => x(Date.parse(d.date)))
+            .y(d => y(parseFloat(d.close))))
 })
+
+
 
